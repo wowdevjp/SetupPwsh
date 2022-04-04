62a4f27d1eacfb62fb6fa72f50e5e9b2594cf008
takeshi-3 <g.jem.beleeem8000@gmail.com>
2022-04-04 13:40:19 +0900

    add key existence check

--- Scripts/EnableAutoLogin.ps1
+++ Scripts/EnableAutoLogin.ps1
@@ -1,14 +1,20 @@
 $Path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
-$Username = Read-Host "User Name"
+$Username = Read-Host "Username"
 $Password = Read-Host "Password"
 
-New-ItemProperty -Path $Path -Name AutoAdminLogon -PropertyType String -Value 1 -Force
-New-ItemProperty -Path $Path -Name DefaultUserName -PropertyType String -Value $Username -Force
-New-ItemProperty -Path $Path -Name DefaultPassword -PropertyType String -Value $Password -Force
-New-ItemProperty -Path $Path -Name ForceAutoLogon -PropertyType String -Value 1 -Force
-New-ItemProperty -Path $Path -Name DefaultDomainName -PropertyType String -Value WORDGROUP -Force
-New-ItemProperty -Path $Path -Name IgnoreShiftOverride -PropertyType String -Value 1 -Force
+function TrySetItemProperty($Path, $Name, $Value) {
+    $Key = Get-ItemProperty -Path $Path -Name $Name -ErrorAction SilentlyContinue
 
-Set-ItemProperty $Path 'AutoAdminLogon' -Value "1"
-Set-ItemProperty $Path 'DefaultUsername' -Value $Username
-Set-ItemProperty $Path 'DefaultPassword' -Value $Password
+    if ($Key -eq $null) {
+        New-ItemProperty -Path $Path -Name $Name -PropertyType String -Value $Value
+    } else {
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value
+    }
+}
+
+TrySetItemProperty -Path $Path -Name AutoAdminLogon -PropertyType String -Value 1
+TrySetItemProperty -Path $Path -Name DefaultUserName -PropertyType String -Value $Username
+TrySetItemProperty -Path $Path -Name DefaultPassword -PropertyType String -Value $Password
+TrySetItemProperty -Path $Path -Name ForceAutoLogon -PropertyType String -Value 1
+TrySetItemProperty -Path $Path -Name DefaultDomainName -PropertyType String -Value WORDGROUP
+TrySetItemProperty -Path $Path -Name IgnoreShiftOverride -PropertyType String -Value 1

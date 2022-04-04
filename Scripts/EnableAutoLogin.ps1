d15a2bfb8aef40064678180da93e81f58fd2d24e
takeshi-3 <g.jem.beleeem8000@gmail.com>
2022-04-04 12:41:33 +0900

    change reg.exe style into powershell style

--- Scripts/EnableAutoLogin.ps1
+++ Scripts/EnableAutoLogin.ps1
@@ -1,14 +1,13 @@
-$RegistryPath = "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $Path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $Username = Read-Host "User Name"
 $Password = Read-Host "Password"
 
-reg add $RegistryPath /v AutoAdminLogon /t REG_SZ /d 1 /f
-reg add $RegistryPath /v DefaultUserName /t REG_SZ /d $Username /f
-reg add $RegistryPath /v DefaultPassword /t REG_SZ /d $Password /f
-reg add $RegistryPath /v ForceAutoLogon /t REG_SZ /d 1 /f
-reg add $RegistryPath /v DefaultDomainName /t REG_SZ /d WORDGROUP /f
-reg add $RegistryPath /v IgnoreShiftOverride /t REG_SZ /d 1 /f
+New-ItemProperty -Path $Path -Name AutoAdminLogon -PropertyType String -Value 1 -Force
+New-ItemProperty -Path $Path -Name DefaultUserName -PropertyType String -Value $Username -Force
+New-ItemProperty -Path $Path -Name DefaultPassword -PropertyType String -Value $Password -Force
+New-ItemProperty -Path $Path -Name ForceAutoLogon -PropertyType String -Value 1 -Force
+New-ItemProperty -Path $Path -Name DefaultDomainName -PropertyType String -Value WORDGROUP -Force
+New-ItemProperty -Path $Path -Name IgnoreShiftOverride -PropertyType String -Value 1 -Force
 
 Set-ItemProperty $Path 'AutoAdminLogon' -Value "1"
 Set-ItemProperty $Path 'DefaultUsername' -Value $Username

ba6036a1bfac3910308a3a22f0c30780ac830a70
umetaman <yuyaumeta@gmail.com>
2024-02-20 14:20:03 +0900

    Set-Service

--- Scripts/InstallOpenSSHServer.ps1
+++ Scripts/InstallOpenSSHServer.ps1
@@ -3,6 +3,10 @@ $choice = Read-Host "Do you want to install OpenSSH Server? (y/n)"
 if ($choice -eq "Y") {
     Write-Host "Installing OpenSSH Server..."
     Add-WindowsCapability -Online -Name OpenSSH.Server~~~~
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+    Get-NetFirewallRule -Name *ssh*
+    Write-Host "OpenSSH Server installed successfully."
 } else {
     Write-Host "OpenSSH Server installation cancelled."
 }
